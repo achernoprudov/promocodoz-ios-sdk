7fa9e60c220d24e580f8c56a74c3cee4f882e5ff
Andrey Chernoprudov <achernoprudov@naumen.ru>
2017-01-27 11:13:30 +0500

    Initial Commit

---
+++ PromocodozSDK/PromocodozSDK.h
@@ -0,0 +1,19 @@
+//
+//  PromocodozSDK.h
+//  PromocodozSDK
+//
+//  Created by Андрей Чернопрудов on 27/01/2017.
+//  Copyright © 2017 Promocodoz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PromocodozSDK.
+FOUNDATION_EXPORT double PromocodozSDKVersionNumber;
+
+//! Project version string for PromocodozSDK.
+FOUNDATION_EXPORT const unsigned char PromocodozSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PromocodozSDK/PublicHeader.h>
+
+
